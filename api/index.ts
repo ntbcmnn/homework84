148421714d3f97996b758cf8a625f2a82c40c502
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-13 13:57:46 +0600

    Imported users and tasks routers

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import config from './config';
+import usersRouter from './routers/users';
+import tasksRouter from './routers/tasks';
 
 const app = express();
 const port = 8000;
@@ -9,6 +11,9 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+
+app.use('/users', usersRouter);
+app.use('/tasks', tasksRouter);
 
 const run = async () => {
     await mongoose.connect(config.db);
